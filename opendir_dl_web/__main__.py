125ae70c6554b903f27843564b6c39dfa8498039
bplower <bplower@gmail.com>
2017-04-08 15:03:43 -0800

    Fixed __main__ to run local directory source
    This will mean running python ./opendir_dl_web from the local source won't run the package installed in production

--- opendir_dl_web/__main__.py
+++ opendir_dl_web/__main__.py
@@ -1,4 +1,13 @@
+import os
 import sys
+
+# This will take and insert the parent directory
+par_dir_list = os.path.realpath(__file__).split('/')[:-2]
+par_dir = "/" + os.path.join(*par_dir_list)
+sys.path.insert(0, par_dir)
+
+# Import the package from the path we just inserted
 import opendir_dl_web
 
+# Now call the main function of the library, providing any values we were provided with
 opendir_dl_web.main(sys.argv[1:])
