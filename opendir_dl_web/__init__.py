db60260085229ac15f6c181dbd7e58b66fd6f7c2
bplower <bplower@gmail.com>
2017-04-24 16:50:03 -0800

    Finished js implementation of pagination
    The js implementation of paginiation is working now. Moved the css to it's own file too. JS could use some improvements I think, since it's pretty rough to look at...

--- opendir_dl_web/__init__.py
+++ opendir_dl_web/__init__.py
@@ -7,50 +7,50 @@ from flask import render_template
 from flask import send_from_directory
 import opendir_dl
 
-class Pagination(object):
-    def __init__(self):
-        self.current_page = 0
-        self.total_pages = 10
-        self.enable_button_step = False
-        self.enable_button_jump = True
-        self.num_buttons = 5
-
-        if not self.enable_button_step and not self.enable_button_jump and self.total_pages < 5:
-            raise ValueError("Enabling step and jump buttons requires 5 or more total buttons. Currently {}".format(self.num_buttons))
-        if (not self.enable_button_step or not self.enable_button_jump) and self.total_pages < 3:
-            raise ValueError("Enabling step or jump buttons requires 3 or more total buttons. Currently {}".format(self.num_buttons))
-
-    def displayed_page_numbers(self):
-        num_navigation_buttons = 2 * sum([self.enable_button_jump, self.enable_button_step])
-        num_page_buttons = self.num_buttons - num_navigation_buttons
-        if num_page_buttons > self.total_pages:
-            num_page_buttons = self.total_pages
-        current_page_position = int(math.ceil(num_page_buttons/2.0))
-        if self.current_page < current_page_position:
-            # Check for begining page postion
-            current_page_position = self.current_page
-        elif self.current_page > self.total_pages - current_page_position:
-            # Check for end page position
-            current_page_position = num_page_buttons - (self.total_pages - self.current_page)
-        value_start_range = self.current_page - current_page_position + 1
-        value_page_range = []
-        for i in range(value_start_range, value_start_range + num_page_buttons):
-            value_page_range.append({"type": "page", "number": i, "value": str(i)})
-        return value_page_range
-
-    def render_list(self):
-        value_range = self.displayed_page_numbers()
-        if self.enable_button_step:
-            value_range.insert(0, {"type": "step", "number": self.current_page - 1, "value": "<"})
-            value_range.append({"type": "step", "number": self.current_page + 1, "value": ">"})
-        if self.enable_button_jump:
-            value_range.insert(0, {"type": "jump", "number": 1, "value": "<<"})
-            value_range.append({"type": "jump", "number": self.total_pages, "value": ">>"})
-        return value_range
-
-    def print_range(self):
-        for i in self.render_list():
-            print i
+# class Pagination(object):
+#     def __init__(self):
+#         self.current_page = 0
+#         self.total_pages = 10
+#         self.enable_button_step = False
+#         self.enable_button_jump = True
+#         self.num_buttons = 5
+#
+#         if not self.enable_button_step and not self.enable_button_jump and self.total_pages < 5:
+#             raise ValueError("Enabling step and jump buttons requires 5 or more total buttons. Currently {}".format(self.num_buttons))
+#         if (not self.enable_button_step or not self.enable_button_jump) and self.total_pages < 3:
+#             raise ValueError("Enabling step or jump buttons requires 3 or more total buttons. Currently {}".format(self.num_buttons))
+#
+#     def displayed_page_numbers(self):
+#         num_navigation_buttons = 2 * sum([self.enable_button_jump, self.enable_button_step])
+#         num_page_buttons = self.num_buttons - num_navigation_buttons
+#         if num_page_buttons > self.total_pages:
+#             num_page_buttons = self.total_pages
+#         current_page_position = int(math.ceil(num_page_buttons/2.0))
+#         if self.current_page < current_page_position:
+#             # Check for begining page postion
+#             current_page_position = self.current_page
+#         elif self.current_page > self.total_pages - current_page_position:
+#             # Check for end page position
+#             current_page_position = num_page_buttons - (self.total_pages - self.current_page)
+#         value_start_range = self.current_page - current_page_position + 1
+#         value_page_range = []
+#         for i in range(value_start_range, value_start_range + num_page_buttons):
+#             value_page_range.append({"type": "page", "number": i, "value": str(i)})
+#         return value_page_range
+#
+#     def render_list(self):
+#         value_range = self.displayed_page_numbers()
+#         if self.enable_button_step:
+#             value_range.insert(0, {"type": "step", "number": self.current_page - 1, "value": "<"})
+#             value_range.append({"type": "step", "number": self.current_page + 1, "value": ">"})
+#         if self.enable_button_jump:
+#             value_range.insert(0, {"type": "jump", "number": 1, "value": "<<"})
+#             value_range.append({"type": "jump", "number": self.total_pages, "value": ">>"})
+#         return value_range
+#
+#     def print_range(self):
+#         for i in self.render_list():
+#             print i
 
 class WebSearchCommand(opendir_dl.commands.BaseCommand):
     valid_options = []
@@ -95,7 +95,10 @@ class OpendirDlWeb(Flask):
 
     def api_search(self):
         results_per_page = 20
-        page = int(request.form.get('page', 1))
+        if request.form.get('page') == 'undefined':
+            page = 1
+        else:
+            page = int(request.form.get('page', 1))
         query = request.form.get('q', '')
         # Handle user input
         input_values = query.split(' ')
@@ -124,45 +127,47 @@ class OpendirDlWeb(Flask):
                 'last_modified': str(i.last_modified)
             })
         return_dict = {
-            'pages': num_pages,
+            'query': query,
+            'page': page,
+            'num_pages': num_pages,
             'results_per_page': results_per_page,
             'results_total': len(results),
             'results': result_list
         }
         return json.dumps(return_dict)
 
-    def results(self):
-        results_per_page = 20
-        page = int(request.args.get('page', 1))
-        query = request.args.get('q', '')
-        # Handle user input
-        input_values = query.split(' ')
-        # Prepare the search command and execute the search
-        command_instance = WebSearchCommand()
-        command_instance.config = self.opendirdl_config
-        command_instance.flags = ["inclusive"]
-        command_instance.values = input_values
-        results = command_instance.run()
-        if len(results) > results_per_page:
-            start_index = (page - 1) * results_per_page
-            end_index = start_index + results_per_page
-            displayed_results = results[start_index: end_index]
-        else:
-            displayed_results = results
-        num_pages = int(math.ceil(float(len(results))/results_per_page))
-        # Build and configure the pagination tracker
-        pages = Pagination()
-        pages.enable_button_step = True
-        pages.current_page = page
-        pages.total_pages = num_pages
-        pages.num_buttons = 11
-        # Now render and return the page
-        return render_template('search.html',
-            paginator = pages,
-            results=displayed_results,
-            num_results_per_page=results_per_page,
-            num_total_results=len(results),
-            query=query)
+    # def results(self):
+    #     results_per_page = 20
+    #     page = int(request.args.get('page', 1))
+    #     query = request.args.get('q', '')
+    #     # Handle user input
+    #     input_values = query.split(' ')
+    #     # Prepare the search command and execute the search
+    #     command_instance = WebSearchCommand()
+    #     command_instance.config = self.opendirdl_config
+    #     command_instance.flags = ["inclusive"]
+    #     command_instance.values = input_values
+    #     results = command_instance.run()
+    #     if len(results) > results_per_page:
+    #         start_index = (page - 1) * results_per_page
+    #         end_index = start_index + results_per_page
+    #         displayed_results = results[start_index: end_index]
+    #     else:
+    #         displayed_results = results
+    #     num_pages = int(math.ceil(float(len(results))/results_per_page))
+    #     # Build and configure the pagination tracker
+    #     pages = Pagination()
+    #     pages.enable_button_step = True
+    #     pages.current_page = page
+    #     pages.total_pages = num_pages
+    #     pages.num_buttons = 11
+    #     # Now render and return the page
+    #     return render_template('search.html',
+    #         paginator = pages,
+    #         results=displayed_results,
+    #         num_results_per_page=results_per_page,
+    #         num_total_results=len(results),
+    #         query=query)
 
 def main(arg_list):
     try:
--- opendir_dl_web/static/app.js
+++ opendir_dl_web/static/app.js
@@ -1,9 +1,14 @@
 
-class SearchPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.results = [{result_name: 'testing!!'}]
+class SearchResultDisplay extends React.Component {
+  render() {
+    var results_start = (this.props.data.page - 1) * this.props.data.results_per_page + 1;
+    var results_end = (this.props.data.page - 1) * this.props.data.results_per_page + this.props.data.results.length;
+    var paragraph_text = "Showing results " + results_start + " through " + results_end + " of " + this.props.data.results_total + " for '" + this.props.data.query + "'";
+    return <div><p>{paragraph_text}</p></div>;
   }
+}
+
+class SearchResults extends React.Component {
   render() {
     return (
       <div>
@@ -21,4 +26,124 @@ class SearchPage extends React.Component {
       </div>
     );
   }
+}
+
+class Paginator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.calculate_buttons = this.calculate_buttons.bind(this);
+    this.get_uri = this.get_uri.bind(this);
+    this.state = {
+      current_page: this.props.data.page,
+      num_pages: this.props.data.num_pages,
+      enable_button_step: true,
+      enable_button_jump: true,
+      num_buttons: 11
+    };
+  }
+
+  get_uri(page) {
+    return "/results?q=" + this.props.data.query + "&page=" + page
+  }
+
+  calculate_buttons() {
+    var num_navigation_buttons = 2 * (this.state.enable_button_jump + this.state.enable_button_step);
+    var num_page_buttons = this.state.num_buttons - num_navigation_buttons;
+
+    if (num_page_buttons > this.state.num_pages) {
+      num_page_buttons = this.state.num_pages;
+    }
+    var current_page_position = Math.ceil(num_page_buttons/2.0);
+    if (this.state.current_page < current_page_position) {
+      // Check for begining page postion
+      current_page_position = this.state.current_page;
+    }
+    else if (this.state.current_page > this.state.num_pages - current_page_position) {
+      // Check for end page position
+      current_page_position = num_page_buttons - (this.state.num_pages - this.state.current_page);
+    }
+    var value_start_range = this.state.current_page - current_page_position + 1;
+    var value_page_range = [];
+    //for i in range(value_start_range, value_start_range + num_page_buttons) {
+    for (var i = value_start_range; i < value_start_range + num_page_buttons; i++) {
+      if (this.state.current_page==i) {
+        value_page_range.push({classes: "active", display_value: i, uri: this.get_uri(i)});
+      }
+      else {
+        value_page_range.push({classes: "", display_value: i, uri: this.get_uri(i)});
+      }
+      //value_page_range.push('<a class="btn btn-primary" style="width: 45px" href="/results?q={ query }&page={i}">{i}</a>');
+    }
+
+    // Make step buttons if enabled
+    if (this.state.enable_button_step) {
+      // next button
+      if (this.state.current_page == this.state.num_pages) {
+        // This means we can't step down a page. Disable the button
+        value_page_range.push({classes: "disabled", display_value: ">", uri: this.get_uri(this.state.current_page)})
+      } else {
+        value_page_range.push({classes: "", display_value: ">", uri: this.get_uri(this.state.current_page + 1)})
+      }
+
+      // prev button
+      if (this.state.current_page == 1) {
+        // This means we can't step down a page. Disable the button
+        value_page_range.splice(0, 0, {classes: "disabled", display_value: "<", uri: this.get_uri(this.state.current_page)})
+      } else {
+        value_page_range.splice(0, 0, {classes: "", display_value: "<", uri: this.get_uri(this.state.current_page - 1)})
+      }
+    }
+
+    // Make jump buttons if enabled
+    if (this.state.enable_button_jump) {
+      // last button
+      if (this.state.current_page == this.state.num_pages) {
+        // This means we can't step down a page. Disable the button
+        value_page_range.push({classes: "disabled", display_value: ">>", uri: this.get_uri(1)})
+      } else {
+        value_page_range.push({classes: "", display_value: ">>", uri: this.get_uri(this.state.num_pages)})
+      }
+
+      // first button
+      if (this.state.current_page == 1) {
+        // This means we can't step down a page. Disable the button
+        value_page_range.splice(0, 0, {classes: "disabled", display_value: "<<", uri: this.get_uri(1)})
+      } else {
+        value_page_range.splice(0, 0, {classes: "", display_value: "<<", uri: this.get_uri(1)})
+      }
+    }
+    return value_page_range;
+  }
+
+  render() {
+    return (
+      <div>
+      <ul className="pagination">
+      {this.calculate_buttons().map(button => (
+        //<a class="btn btn-primary" style="width: 45px" href="/results?q={ query }&page={button.page_num}">{button.page_num}</a>
+        // <li className={button.classes}><a style={{width: 45}} href={button.uri}>{button.display_value}</a></li>
+        <li className={button.classes}><a href={button.uri}>{button.display_value}</a></li>
+      ))}
+      </ul>
+      </div>
+    );
+  }
+}
+
+// <div style="text-align: center; margin: 20px">
+// {% for button in paginator.render_list() %}
+//   {% if button.type == "page" and button.number == paginator.current_page %}
+//   <a class="btn btn-primary" style="width: 45px" href="/results?q={{ query }}&page={{ button.number }}" disabled>{{ button.value }}</a>
+//   {% elif button.type == "jump" or button.type == "step" %}
+//   <a class="btn btn-default" href="/results?q={{ query }}&page={{ button.number }}">{{ button.value }}</a>
+//   {% else %}
+//   <a class="btn btn-primary" style="width: 45px" href="/results?q={{ query }}&page={{ button.number }}">{{ button.value }}</a>
+//   {% endif %}
+// {% endfor %}
+// </div>
+
+function get_param(name){
+  // Retrieve the value for a variable defined by 'name'
+  if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+    return decodeURIComponent(name[1]);
 }
